23186f3d2779df2aba3e93d0a9edcf731ccbf3cb
Kawika Avilla <kavilla414@gmail.com>
2024-08-19 05:38:18 +0000

    brain dump
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

--- src/plugins/data/public/query/query_string/dataset_manager/dataset_manager.ts
+++ src/plugins/data/public/query/query_string/dataset_manager/dataset_manager.ts
@@ -6,16 +6,39 @@
 import { BehaviorSubject } from 'rxjs';
 import { CoreStart } from 'opensearch-dashboards/public';
 import { skip } from 'rxjs/operators';
-import { DEFAULT_QUERY, Dataset, DataSource, IndexPattern, UI_SETTINGS } from '../../../../common';
+import {
+  Dataset,
+  DataSource,
+  IndexPattern,
+  UI_SETTINGS,
+  DEFAULT_DATA,
+  DataStructure,
+  CachedDataStructure,
+  DataStructureCache,
+  toCachedDataStructure,
+  createDataStructureCache,
+} from '../../../../common';
 import { IndexPatternsContract } from '../../../index_patterns';
+
+type DatasetTypeHandler = (dataset: Dataset) => DataStructure;
 
 export class DatasetManager {
   private dataset$: BehaviorSubject<Dataset | undefined>;
   private indexPatterns?: IndexPatternsContract;
   private defaultDataset?: Dataset;
+  private dataStructureCache: DataStructureCache;
+  private datasetTypeRegistry: Map<string, DatasetTypeHandler>;
 
   constructor(private readonly uiSettings: CoreStart['uiSettings']) {
     this.dataset$ = new BehaviorSubject<Dataset | undefined>(undefined);
+    this.dataStructureCache = createDataStructureCache();
+    this.datasetTypeRegistry = new Map();
+
+    // Register default handlers
+    this.registerDatasetType(
+      DEFAULT_DATA.SET_TYPES.INDEX_PATTERN,
+      this.indexPatternToDataStructure
+    );
   }
 
   public init = async (indexPatterns: IndexPatternsContract) => {
@@ -30,21 +53,7 @@ export class DatasetManager {
       return undefined;
     }
 
-    this.defaultDataset = {
-      id: indexPattern.id,
-      title: indexPattern.title,
-      type: DEFAULT_QUERY.DATASET_TYPE,
-      timeFieldName: indexPattern.timeFieldName,
-      ...(indexPattern.dataSourceRef
-        ? {
-            dataSource: {
-              id: indexPattern.dataSourceRef?.id,
-              title: indexPattern.dataSourceRef?.name,
-              type: indexPattern.dataSourceRef?.type,
-            } as DataSource,
-          }
-        : {}),
-    };
+    this.defaultDataset = this.createDatasetFromIndexPattern(indexPattern);
   };
 
   public getUpdates$ = () => {
@@ -55,13 +64,13 @@ export class DatasetManager {
     return this.dataset$.getValue();
   };
 
-  /**
-   * Updates the query.
-   * @param {Query} query
-   */
-  public setDataset = (dataSet: Dataset | undefined) => {
+  public setDataset = (dataset: Dataset | undefined) => {
     if (!this.uiSettings.get(UI_SETTINGS.QUERY_ENHANCEMENTS_ENABLED)) return;
-    this.dataset$.next(dataSet);
+    this.dataset$.next(dataset);
+
+    if (dataset) {
+      this.cacheDatasetStructure(dataset);
+    }
   };
 
   public getDefaultDataset = () => {
@@ -79,22 +88,79 @@ export class DatasetManager {
       return undefined;
     }
 
-    return {
-      id: indexPattern.id,
-      title: indexPattern.title,
-      type: DEFAULT_QUERY.DATASET_TYPE,
-      timeFieldName: indexPattern.timeFieldName,
-      ...(indexPattern.dataSourceRef
-        ? {
-            dataSource: {
-              id: indexPattern.dataSourceRef?.id,
-              title: indexPattern.dataSourceRef?.name,
-              type: indexPattern.dataSourceRef?.type,
-            } as DataSource,
-          }
-        : {}),
-    };
+    return this.createDatasetFromIndexPattern(indexPattern);
   };
+
+  private createDatasetFromIndexPattern(indexPattern: IndexPattern): Dataset {
+    return {
+      id: indexPattern.id!,
+      title: indexPattern.title,
+      type: DEFAULT_DATA.SET_TYPES.INDEX_PATTERN,
+      timeFieldName: indexPattern.timeFieldName,
+      dataSource: indexPattern.dataSourceRef
+        ? {
+            id: indexPattern.dataSourceRef.id,
+            title: indexPattern.dataSourceRef.name!,
+            type: indexPattern.dataSourceRef.type,
+          }
+        : undefined,
+    };
+  }
+
+  private indexPatternToDataStructure(dataset: Dataset): DataStructure {
+    return {
+      id: dataset.id,
+      title: dataset.title,
+      type: DATA_STRUCTURE_TYPES.INDEX,
+      parent: dataset.dataSource
+        ? {
+            id: dataset.dataSource.id,
+            title: dataset.dataSource.title,
+            type: DATA_STRUCTURE_TYPES.DATA_SOURCE,
+          }
+        : undefined,
+    };
+  }
+
+  public registerDatasetType(type: string, handler: DatasetTypeHandler) {
+    this.datasetTypeRegistry.set(type, handler);
+  }
+
+  private cacheDatasetStructure(dataset: Dataset) {
+    const handler = this.datasetTypeRegistry.get(dataset.type);
+    if (!handler) {
+      console.warn(`No handler registered for dataset type: ${dataset.type}`);
+      return;
+    }
+
+    const dataStructure = handler(dataset);
+    this.cacheDataStructureRecursively(dataStructure);
+  }
+
+  private cacheDataStructureRecursively(dataStructure: DataStructure) {
+    const cachedStructure = toCachedDataStructure(dataStructure);
+    this.dataStructureCache.set(cachedStructure.id, cachedStructure);
+
+    if (dataStructure.parent) {
+      this.cacheDataStructureRecursively(dataStructure.parent);
+    }
+
+    if (dataStructure.children) {
+      dataStructure.children.forEach((child) => this.cacheDataStructureRecursively(child));
+    }
+  }
+
+  public getCachedDataStructure(id: string): CachedDataStructure | undefined {
+    return this.dataStructureCache.get(id);
+  }
+
+  public clearDataStructureCache(id?: string) {
+    if (id) {
+      this.dataStructureCache.clear(id);
+    } else {
+      this.dataStructureCache.clearAll();
+    }
+  }
 }
 
 export type DatasetContract = PublicMethodsOf<DatasetManager>;
